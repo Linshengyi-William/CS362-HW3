187226df227af55781e5b0fe4e14538d41a2b271
Linshengyi-William <sunlins@oregonstate.edu>
2021-04-25 00:12:00 -0700

    leap year program without error handling finished

---
+++ leapYear.py
@@ -0,0 +1,12 @@
+#code adapted from:https://www.javatpoint.com/python-check-leap-year
+year = int(input("Enter a year: "))  
+if (year % 4) == 0:  
+   if (year % 100) == 0:  
+       if (year % 400) == 0:  
+           print(year,"is a leap year")  
+       else:  
+           print(year,"is not a leap year")  
+   else:  
+       print(year,"is a leap year")  
+else:  
+   print(year,"is not a leap year")  
